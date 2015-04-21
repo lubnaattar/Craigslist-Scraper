b10b3c858aa2e67308b6ccb83ef32970fb2faa40
devbot <kavishb.parikh@gmail.com>
2015-04-21 15:58:48 -0700

    Graphs on Dashboard, Image Slider

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,12 @@
 class DashboardController < ApplicationController
   def home
 
-  #  @posts = Post.all
-     @posts = Post.group(:city).count #city,  #count_all
-     @posts1 = Post.group(:year).count #year, #count_all
+    #  @posts = Post.all
+    #  @posts = Post.group(:city).count #city,  #count_all
+    #  @posts1 = Post.group(:year).count #year, #count_all
+     @posts = Post.where("city like ?", "Anaheim").group(:year).count.to_a
+     @posts1 = Post.group(:year).count.to_a #year, #count_all
+     @posts2 = Post.group(:city).count.to_a
+     @posts3 = Post.group(:make_vehicle).count.to_json
   end
 end
